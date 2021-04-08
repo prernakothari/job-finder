45d0bd073887dc13da068578c5b9c1e397c4a63d
Prerna <prernakotharius@gmail.com>
2021-04-08 06:03:20 +0530

    adding constants.js

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const colors = {
+    bgLight: "#F5F6F8",
+    bgDark: "#131822",
+    mainLight: "#FFFFFF",
+    mainDark: "#19212D",
+    purple: "#5865E0",
+    darkPurple: "#3D469C",
+    lightPurple: "#CECFF6"
+}
